e23da62d8b17735a4c26b7830073dddac17dc92b
ALBAB404 <imalbab4@gmail.com>
2023-01-19 23:09:27 +0600

    CRUD

--- Admin/core/insert.php
+++ Admin/core/insert.php
@@ -1,44 +1,44 @@
-<?php
+<?php 
+
 include '../inc/connection.php';
-// insert category 
 
 if (isset($_POST['add_category'])) {
-    $name = $_POST['cat_name'];
+    $cat_name = $_POST['cat_name'];
     $is_parent = $_POST['is_parent'];
     $file_name = $_FILES['choose-file']['name'];
     $tmp_name = $_FILES['choose-file']['tmp_name'];
-    $file_size = $_FILES['choose-file']['size'];
 
-    $splited_files = explode('.',$file_name);
-    $extn = strtolower(end($splited_files));
-
-    $extension = array('png','jpg','jpeg');
-
-    if (!empty($file_name)) {
-        if (in_array($extn,$extension) === true) {
+    $split = explode('.',$file_name);
+    $extn = strtolower(end($split));
+    $compaire = array('jpg','png');
+    
+    if (!empty($cat_name)) {
+        if (in_array($extn,$compaire) === true) {
+        
             $update_name = rand().$file_name;
-            move_uploaded_file($tmp_name,'../assets/img/products/category/'.$update_name);
-            $cat_insert = "INSERT INTO fashion_category (f_name,is_parent,f_status) value ('$name','$is_parent','1')";
-            $cat_insert_res = mysqli_query($db,$cat_insert);
-            if ($cat_insert_res) {
-            header('location: ../category.php');
-            }else{
-                die('error in database!'.mysqli_error($db));
-            }
-                }else {
-                    echo "warning ! please upload a (jpg,png,jpeg) files !";
-                }
-            }else {
-                $cat_insert = "INSERT INTO fashion_category (f_name,is_parent,f_status) value ('$name','$is_parent','1')";
-                $cat_insert_res = mysqli_query($db,$cat_insert);
-                if ($cat_insert_res) {
+            move_uploaded_file($tmp_name,'../assets/img/icon/'.$update_name);
+            $insert_sql =  "INSERT INTO fashion_category (f_name,f_image,is_parent,f_status) VALUE ('$cat_name','$update_name','$is_parent','1')";
+             $insert_res = mysqli_query($db,$insert_sql);
+            if ($insert_res) {
                 header('location: ../category.php');
-        }else{
-            die('error in database!'.mysqli_error($db));
+            }else{
+                die('error from insert page!'.mysqli_error($db));
+            }
+        }else {
+            $insert_sql =  "INSERT INTO fashion_category (f_name,is_parent,f_status) VALUE ('$cat_name','$is_parent','1')";
+        $insert_res = mysqli_query($db,$insert_sql);
+            if ($insert_res) {
+                header('location: ../category.php');
+            }else{
+                die('error from insert page!'.mysqli_error($db));
+            }
         }
+    }else {
+        header('location: ../category.php');
+        die("<script>alert('Please Fill The Gap')</script>".mysqli_error($db));
+
     }
-
+  
 }
-
 
 ?>
